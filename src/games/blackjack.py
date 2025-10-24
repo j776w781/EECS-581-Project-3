33ef93cc7cb18e19febd9c8acd532dc83e7b220b
Josh Welicky <joshwelicky@gmail.com>
2025-10-24 12:01:02 -0500

    Fixed font bugs

--- src/games/blackjack.py
+++ src/games/blackjack.py
@@ -195,8 +195,23 @@ class BlackJackScreen(QWidget):
 
         print(f"Dealer's score: {self.game.dealerScore}")
 
+
+    def game_over(self, winner):
+        if winner == "dealer":
+            '''
+            BISSHOY: The player loses the chips they bet "stored in self.game.chips". You'll have to subtract those from
+            the player_chips attribute. This would be the time to potentially remove them from the game. Also, might want to include
+            in here the option to replay(reset the GUI to initial state).
+            '''
+            return
+        return
+
+
+
+
     def hit(self):
         print("IMPLEMENT ME")
+        cur_total, busted = self.game.hit("human")
         '''
         TODO: Use the BlackJack class (.game attribute) to add a card to the player's hand.
         DON'T do the random selection here. It should do that in the BlackJack class and simply
@@ -204,9 +219,13 @@ class BlackJackScreen(QWidget):
 
         This function should ONLY handle the animation for adding the card to the DEALER's OR PLAYER's hand.
         It could also trigger the loss function if bust returns true.
-        '''
-        return
 
+        Could also show current score.
+        '''
+        if busted:
+            self.game_over("dealer")
+        return
+    
     def start(self):
 
         if self.game.chips > 0:
@@ -276,37 +295,6 @@ class BlackJack:
         return final
 
 
-
-
-    def playerTurn(self):
-        while True:
-            print("\nDealer's hand:")
-            self.printHand(self.dealerHand, True)
-            print("\nPlayer's hand:")
-            self.printHand(self.playerHand)
-
-            choice = input('\nHit or Stand?: ')
-            choices = ['hit', 'h', 'stand', 's']
-            while choice.lower() not in choices:
-                print("Invalid choice. Please 'hit' or 'stand'.")
-                choice = input('Hit or Stand: ')
-
-            if choice.lower() in ['hit', 'h']:
-                self.playerHand.append(self.deck.draw())
-                total = self.getTotal(self.playerHand)
-
-                if len(total) > 1:
-                    self.verifyTotal(total)
-                if total[0] > 21:
-                    total = self.getBestSum(total)
-                    return total
-            else:
-                total = self.getTotal(self.playerHand)
-                if len(total) > 1:
-                    self.verifyTotal(total)
-                total = self.getBestSum(total)
-                return total
-            
     def hit(self, player):
         if player == 'human':
             self.playerHand.append(self.deck.draw())
@@ -314,20 +302,10 @@ class BlackJack:
 
             if len(total) > 1:
                 self.verifyTotal(total)
-            if total[0] > 21:
-                total = self.getBestSum(total)
-                return total
-        else:
-            self.dealerHand.append(self.deck.draw())
-            total = self.getTotal(self.playerHand)
-
-            if len(total) > 1:
-                self.verifyTotal(total)
-            if total[0] > 21:
-                total = self.getBestSum(total)
-                return total
-
-
+            total = self.getBestSum(total)
+            if total > 21:
+                return total, True
+            return total, False
 
 
     def dealerTurn(self):
@@ -387,6 +365,38 @@ class BlackJack:
                 return total
         '''
 
+
+
+    def playerTurn(self):
+        while True:
+            print("\nDealer's hand:")
+            self.printHand(self.dealerHand, True)
+            print("\nPlayer's hand:")
+            self.printHand(self.playerHand)
+
+            choice = input('\nHit or Stand?: ')
+            choices = ['hit', 'h', 'stand', 's']
+            while choice.lower() not in choices:
+                print("Invalid choice. Please 'hit' or 'stand'.")
+                choice = input('Hit or Stand: ')
+
+            if choice.lower() in ['hit', 'h']:
+                self.playerHand.append(self.deck.draw())
+                total = self.getTotal(self.playerHand)
+
+                if len(total) > 1:
+                    self.verifyTotal(total)
+                if total[0] > 21:
+                    total = self.getBestSum(total)
+                    return total
+            else:
+                total = self.getTotal(self.playerHand)
+                if len(total) > 1:
+                    self.verifyTotal(total)
+                total = self.getBestSum(total)
+                return total
+            
+    
     def play(self):
         self.deal(self.dealerHand)
         self.deal(self.playerHand)
