ba82a5e5c9b4f53e967bf0fc371cbcb65bdd681b
Gavin Billinger <gbillinger@ku.edu>
2025-11-13 13:18:21 -0600

    Updated opponent.py
    Added some experiment AI behavior. When other mechanics are implemented...should be promising.

--- src/games/objects/opponent.py
+++ src/games/objects/opponent.py
@@ -27,7 +27,40 @@ class Opponent:
         print(self.handRank)
 
         # For now I just have the AI defaulting to checking.
-        self.game.check(id)
+        hand_strengths = {
+            "High Card": 0.1,
+            "Pair": 0.25,
+            "Two Pair": 0.4,
+            "Three of a Kind": 0.55,
+            "Straight": 0.65,
+            "Flush": 0.75,
+            "Full House": 0.85,
+            "Four of a Kind": 0.9,
+            "Straight Flush": 0.95,
+            "Royal Flush": 1.0,
+        }
+
+        strength = hand_strengths.get(self.handRank, 0.1)
+        strength += random.uniform(-0.1, 0.1)
+
+        if self.game.activeBet:
+            if strength < 0.2:
+                self.game.fold(id) if random.random() < 0.8 else self.game.call(id)
+            elif strength < 0.5:
+                self.game.call(id)
+            elif strength < 0.8:
+                self.game.call(id) if random.random() < 0.7 else self.game._raise(id)
+            else:
+                self.game._raise(id) if random.random() < 0.6 else self.game.call(id)
+        else:
+            self.game.check(id)
+            if strength > 0.75 and random.random() < 0.5:
+                self.game.bet(id)
+            elif strength > 0.5 and random.random() < 0.3:
+                self.game.bet(id)
+            else:
+                self.game.check(id)
+
         '''
         if self.game.activeBet:
             num = random.randint(0, 2)
